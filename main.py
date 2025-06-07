f1c7b58cb93e41d18cd9ec9896e710afca14c949
Tomás Ferraz <tomasnfh1@gmail.com>
2025-06-07 14:10:38 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 tarea = 'descarga de camiones a ibc'
 sustancia = 'soda cáustica'
-Prompt1 = 'Quiero que me brindes un checklist como un archivo .txt de condiciones que tiene que cumplir el operario (ejemplo usar casco, guantes, mascarilla, lentes, etc. ), de realizar la tarea de'+tarea+'de'+sustancia+'en una fabrica de BASF. Quiero que utilices el documento de hoja de seguridad BASF que te adjunto como referencia y la hoja de datos de seguridad de '+sustancia+' para que verifique qué tiene que cumplir el operario (ej> si es GHS05 - corrosiva, que use guantes, mascarilla si es GHS08 Sensibilizante respiratorio , etc.). No quiero que incluyas posibles procedimientos en caso de emergencia, solo que conciderar.'
-print(Prompt1)
+
+def funcion_de_riesgo(tarea, sustancia, condiciones_entorno):
+
+  Prompt1 = 'Quiero que me brindes un checklist como un archivo .txt de condiciones que tiene que cumplir el operario (ejemplo usar casco, guantes, mascarilla, lentes, etc. ), de realizar la tarea de'+tarea+'de'+sustancia+'en una fabrica de BASF. Quiero que utilices el documento de hoja de seguridad BASF que te adjunto como referencia y la hoja de datos de seguridad de '+sustancia+' para que verifique qué tiene que cumplir el operario (ej> si es GHS05 - corrosiva, que use guantes, mascarilla si es GHS08 Sensibilizante respiratorio , etc.). No quiero que incluyas posibles procedimientos en caso de emergencia, solo que conciderar.'
+  print(Prompt1)
 
